820739dc57d247399df81b37b3ecdfeab8db5c4e
dmytro.isaiev <Dmytro.isaiev@codeit.pro>
2026-02-25 14:30:46 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="app">
+            <h1>Travel Test App</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import App from './App'
+import { store } from './app/store'
+import './styles/global.scss'
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>,
+)
